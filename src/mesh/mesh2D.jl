7ba716fb0cfa72a6df889243a7af084e45d25d1b
Max Opgenoord <mopg@mit.edu>
2017-09-14 09:53:02 -0400

    HDG solve elasticity in 2D compiles, yet to test accuracy. P=3 singular

--- src/mesh/mesh2D.jl
+++ src/mesh/mesh2D.jl
@@ -154,6 +154,11 @@ function genFaces2D( t::Array{Int64}, bel::Array{Int64} )
     indel = find( all( bel[:,1:2] .== f[indf,1:2]', 2) )
 
     # Ensure boundary element is oriented counter-clockwise
+    #   flip orientation of face in t2f
+    indt = f[indf,4]
+    indfint = find( abs(t2f[indt,:]) .== indf )
+    t2f[ f[indf,4], indfint ] = - t2f[ f[indf,4], indfint ]
+    #   flip nodes in f
     temp = f[indf,1]
     f[indf,1] = f[indf,2]
     f[indf,2] = temp
