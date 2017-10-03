38bc3694e4f3a95d1f05bee3aed6c5625dd8b44c
Max Opgenoord <mopg@mit.edu>
2017-10-02 21:09:53 -0400

    Updated mesh test for new source

--- test/unit/testMesh.jl
+++ test/unit/testMesh.jl
@@ -1,12 +1,3 @@
-# include("../src/mesh/mesh2D.jl")
-# include("../../src/mesh/mesh2D.jl")
-# include("../../src/mesh/master2D.jl")
-# include("../../src/mesh/mesh3D.jl")
-# include("../../src/mesh/master3D.jl")
-# include("../../src/mesh/compJacob.jl")
-
-# mesh2d = Mesh2D( "square", 2 )
-
 tol = 1e-13
 
 # check boundaries
@@ -44,8 +35,18 @@ for ii in [2,5], jj in [2,5], kk in [2,5], pp in 1:3
 
       for qq in 1:length(nodr), dd in 1:3
         @test abs.(mesh.nodes[ nodr[qq], dd, tr ] -
-                  mesh.nodes[ nodl[qq], dd, tl ]) < tol
-        @printf("%5.4f\t%5.4f\n",mesh.nodes[ nodr[qq], dd, tr ], mesh.nodes[ nodr[qq], dd, tr ])
+                   mesh.nodes[ nodl[qq], dd, tl ]) < tol
+      end
+
+    else
+      # check that nodes are actually on boundary they should be on
+      itrf =   find( mesh.t2f[tr,1:4] .== ff )
+      nodr =   master.perm[ :,itrf,abs.(mesh.t2f[tr,itrf+4]) ]
+      nb   = - mesh.f[ff,end]
+      for qq in 1:length(nodr)
+        ic   = indchecks[ nb ]
+        cval = checkval[  nb ]
+        @test mesh.nodes[ nodr[qq], ic, tr ] == cval
       end
 
     end
@@ -53,7 +54,7 @@ for ii in [2,5], jj in [2,5], kk in [2,5], pp in 1:3
   end
 end
 
-check integration through volume integrals
+# check integration through volume integrals
 for ii in [2,5,7,10], jj in [2,5,7,10], kk in [2,5,7,10], pp in 1:3
 
   mesh = Mesh3D( "cube", pp, N = ii, M = jj, Q = kk )
@@ -92,15 +93,9 @@ for ii in [5,10], jj in [2,10], kk in [2,10], pp in 1:3
     for qq in 1:4
 
       indF = mesh.t2f[el,qq]
-      nods = master.perm[ :, qq, abs.(mesh.t2f[el,qq+4]) ]
 
-      p2d  = master.ϕ2D'  * mesh.nodes[nods,:,el] # rows is points, cols is dimension
+      (ϕ2d,p2d,nods,normal,jcw2D) = luteos.compJacobFace( mesh, master, el, qq )
 
-      (normal,jcw2D) = luteos.compJacobFace( Val{3}, master.∇ϕ2D, master.gwts2D, mesh.nodes[ nods, :, el ] )
-
-      if (mesh.t2f[el,qq+4] < 0)
-        normal = -normal
-      end
       volttemp = 0.0
       for ll in 1:size(p2d,1)
         volttemp += 1/3 * jcw2D[ll] * (normal[ll,:]' * p2d[ll,:])
